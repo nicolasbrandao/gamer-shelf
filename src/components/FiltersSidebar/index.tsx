64b345abffcd8e87417734953b868da62e62366c
Nícolas Brandão <nicolas.brandaor@gmail.com>
2023-04-13 21:11:41 -0300

    fix: fix misalignment on mobile version

--- src/components/FiltersSidebar/index.tsx
+++ src/components/FiltersSidebar/index.tsx
@@ -2,7 +2,7 @@ import classNames from 'classnames'
 import { FiltersSidebarList } from './subcomponents'
 
 const FiltersSidebar = () => {
-  const filtersContainer = classNames('w-[280px]', 'mt-4', 'mr-8')
+  const filtersContainer = classNames('w-[280px]', 'mt-4')
 
   return (
     <nav className={filtersContainer} aria-label="Advanced Filters">
